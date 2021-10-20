303845cc26ff5b2bf85663f86ee8ece457e540ee
jkuchma <jkuchma@udel.edu>
2021-10-20 00:00:26 -0400

    Added functionality to 'Login as User' button and switched some backend things around

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,6 @@ class BackgroundVideo extends StatefulWidget {
   _BackgroundVideoState createState() => _BackgroundVideoState();
 }
 
-class AboutUs extends StatefulWidget {
-  @override
-  _AboutUs createState() => _AboutUs();
-}
-
 class _BackgroundVideoState extends State<BackgroundVideo> {
   // TODO 4: Create a VideoPlayerController object.
   VideoPlayerController _controller;
@@ -155,7 +150,12 @@ class LoginWidget extends StatelessWidget {
                     'Login as User',
                     style: TextStyle(color: Color(0xffffd544), fontSize: 20),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginUser()),
+                    );
+                  },
                 ),
               ),
               TextButton(
@@ -181,22 +181,79 @@ class LoginWidget extends StatelessWidget {
   }
 }
 
-class _AboutUs extends State {
-  VideoPlayerController _controller;
-
+class LoginUser extends StatelessWidget {
+  const LoginUser({
+    Key key,
+  }) : super(key: key);
   @override
-  void initState() {
-    super.initState();
-    // Pointing the video controller to our local asset.
-    _controller = VideoPlayerController.asset("assets/Background_Video.mp4")
-      ..initialize().then((_) {
-        // Once the video has been loaded we play the video and set looping to true.
-        _controller.play();
-        _controller.setLooping(true);
-        // Ensure the first frame is shown after the video is initialized.
-        setState(() {});
-      });
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          child: Center(
+            child: Image(
+              image: AssetImage("assets/IDS_logo.png"),
+              width: 300.0,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 75.0,
+        ),
+        Container(
+          decoration: new BoxDecoration(
+            color: Colors.white.withAlpha(200),
+            borderRadius: new BorderRadius.only(
+              topLeft: const Radius.circular(10.0),
+              topRight: const Radius.circular(10.0),
+              bottomLeft: const Radius.circular(10.0),
+              bottomRight: const Radius.circular(10.0),
+            ),
+          ),
+          padding: EdgeInsets.all(16),
+          width: 300,
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Material(
+                  child: TextField(
+                cursorColor: Color(0xffb55e28),
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                ),
+              )),
+              Material(
+                  child: TextField(
+                cursorColor: Color(0xffb55e28),
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+              )),
+              ButtonTheme(
+                minWidth: 300.0,
+                child: RaisedButton(
+                  color: Color(0xff00539f),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Color(0xffffd544), fontSize: 20),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
+}
+
+class AboutUs extends StatelessWidget {
+  const AboutUs({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
